4014d3a5701aa6edc6d30d623c0241acc13f2f4c
Franco <francoespejo743@gmail.com>
2025-11-11 01:34:34 -0300

    se cambiaron las rutas y la logica de controladores

--- routers/rutinas.routes.js
+++ routers/rutinas.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { infoRutinas } = require('../rutinas');
 
-router.get('/', (req, res) => {
-  res.json(infoRutinas);
-});
+router.get('/', rutinasController.obtenerRutinas);
+router.post('/', rutinasController.crearRutina);
+router.delete('/:id', rutinasController.eliminarRutina);
 
 module.exports = router;
